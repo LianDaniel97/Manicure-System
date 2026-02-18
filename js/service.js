201262849f9f55e3567300c1095b33629fadb828
Neta <netamir98@gmail.com>
2026-02-18 13:32:54 +0200

    feat: build and append service card HTML

--- js/service.js
+++ js/service.js
@@ -13,3 +13,17 @@ for (var i = 0; i < SERVICES.length; i++) {
 
     }
 }
+var card = '<article class="service-card">' +
+'<div class="service-image" style="background-image: url(\'' + service.image + '\')"></div>' +
+'<div class="service-content">' +
+'<h3>' + service.name + '</h3>' +
+'<p>' + service.description + '</p>' +
+'<div class="service-meta">' +
+'<span class="price">₪' + service.price + '</span>' +
+'<span class="duration">' + service.durationMin + ' דק\'</span>' +
+'</div>' +
+'<a href="booking.html?service=' + service.id + '" class="btn-outline">הזמיני תור</a>' +
+'</div>' +
+'</article>';
+
+container.append(card);
